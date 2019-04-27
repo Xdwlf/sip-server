ac0644525c30af228f1f74e6af963c66093b54bd
Rowan Lin <xdwlf32@gmail.com>
2019-04-26 22:10:05 -0700

    set up framework with express with routes and basic error handling

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const errorHandler = require('./handlers/error')
+const PORT = process.env.PORT || 8081
+
+app.use(cors())
+app.use(bodyParser.json())
+
+//Routes
+app.use("/api/auth", authRoutes)
+app.use("/api/user/:id", userRoutes)
+app.use("/api/drinks/:id/comments", commentRoutes)
+
+//If no routes are reached
+app.use(function(req,res, next){
+    let err = new Error("Not Found")
+    err.status = 404
+    return next(err)
+})
+
+app.use(errorHandler)
+
+app.listen(PORT, function(){
+    console.log(`Server has started on port ${PORT}`)
+})
