29261747122e8afed0681e3ae11801ba4da15b59
Rowan Lin <xdwlf32@gmail.com>
2019-04-27 18:36:40 -0700

    export User and Comment models through index.js

--- models/user.js
+++ models/user.js
@@ -16,9 +16,15 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    profileImgUrl: {
+        type: String
+    },
     comments: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: "Comment"
+    }],
+    favoriteDrinks: [{
+        type: String
     }]
 })
 
@@ -27,7 +33,7 @@ userSchema.pre("save", async function(next){
         if(!this.isModified('password')){
             return next()
         }
-        let hashedPassword = await bcrypt.hash(this.password, 100);
+        let hashedPassword = await bcrypt.hash(this.password, 10);
         this.password = hashedPassword
         return next()
     } catch(e){
@@ -36,7 +42,12 @@ userSchema.pre("save", async function(next){
 })
 
 userSchema.methods.comparePassword = async function(candidatePassword, next){
-
+    try{
+        let isMatch = await bcrypt.compare(candidatePassword, this.password)
+        return isMatch
+    } catch(e){
+        return next(e)
+    }
 }
 
 module.exports = mongoose.model("User", userSchema)
